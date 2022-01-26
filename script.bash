3b1af6cfbce88af038ddd0de250629699fa1ec1a
William Sampaio <1997wbms@gmail.com>
2022-01-26 11:21:35 -0400

    update add color

--- script.bash
+++ script.bash
@@ -1,7 +1,15 @@
 #!/bin/bash
 
-echo "Hello $('whoami').";
+#Example: '\033[<background>;<foreground>m'
+GREEN='\033[1;32m';
+YELLOW='\033[0;33m';
+LIGHT_RED='\033[1;31m';
+SSH_KEY_COLOR='\033[44;93m';
+ERROR_COLOR='\033[41;33m';
+NO_COLOR='\033[0m';
 
+clear;
+echo -e "${YELLOW}Hello $('whoami').";
 read -p "Please type your e-mail address: " email;
 
 ssh-keygen -t ed25519 -C $email -f ~/.ssh/$email -N '';
@@ -12,8 +20,8 @@ ssh-add ~/.ssh/$email;
 
 clear;
 
-echo "Your SSH Key was successful generated!";
-echo
+echo -e "${GREEN}Your SSH Key was successful generated!${YELLOW}";
+echo;
 
 while true; do
     read -p "Would you like to add the key to your github account? (y/n): " option
@@ -34,17 +42,18 @@ while true; do
             -d "$data" \
             -o /dev/null);
             if [ "$response" == "201" ]; then
-                echo "SSH Key successful added!";
+                echo -e "${GREEN}SSH Key successful added!${YELLOW}";
             else
-                echo "Error trying to add SSH Key. Status code: $response (see GitHub API documentation).";
+                echo -e "${LIGHT_RED}Error trying to add SSH Key. Status code: ${ERROR_COLOR} ${response} ${NO_COLOR}${LIGHT_RED} (see GitHub API documentation).${YELLOW}";
             fi
             exit 1;;
         [Nn]* )
-            echo "Your SSH key is (Key already copied to clipboard):";
-            echo;
+            echo -e "Your SSH key is ${LIGHT_RED}(Key already copied to clipboard)${YELLOW}:";
+            echo -e $SSH_KEY_COLOR;
             cat ~/.ssh/$email.pub;
+            echo -e $YELLOW;
             cat ~/.ssh/$email.pub | xclip -selection clipboard;
             exit 1;;
-        * ) echo "Please answer yes or no.";;
+        * ) echo -e "${LIGHT_RED}Please answer yes or no.${YELLOW}";;
     esac
 done
